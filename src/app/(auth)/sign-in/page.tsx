7fde37ac2e99dd5b7255a20df8aa0d5a1cd57ac3
HillaryChikendu <chikenduhillarychiagoziemu@gmail.com>
2024-01-17 11:23:32 +0100

    tailwind not working

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => (
+    <div className='bg-blue w-full h-screen'>
+        <p className='text-white font-bold text-3xl text-center'>MentalPaddi</p>
+        <form></form>
+    </div>
+);
+
+export default Page;
